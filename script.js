985d20f77ba57de2017e012689b72cc16aa27db4
ltalken5 <luketalken5@gmail.com>
2026-07-22 20:26:57 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ Promise.all([
 
 .then(([players, schedule]) => {
 
-    const today = new Date().toISOString().split("T")[0];
+   const now = new Date();
+
+const today =
+    now.getFullYear() + "-" +
+    String(now.getMonth() + 1).padStart(2, "0") + "-" +
+    String(now.getDate()).padStart(2, "0");
 
     const todayEntry = schedule.find(item => item.date === today);
 
